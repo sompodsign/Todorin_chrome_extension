a2951947786c37965cbbb5f3251bc9ca3cd05a05
Shampad Sharkar <sompodsign@gmail.com>
2021-08-04 19:05:37 +0600

    loading spinner added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoScreen from './screens/TodoScreen'
-import Header from './components/Header';
 
 
 function App() {
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 import db from '../firebase.config'
 import Card from '../components/Card'
 import GoalInput from '../components/GoalInput';
+import Loader from '../components/Loader';
 
 const date = new Date().getDate(); //To get the Current Date
 const month = new Date().getMonth() + 1; //To get the Current Month
 const year = new Date().getFullYear(); //To get the Current Year
+
 
 function TodoScreen() {
     const [todos, setTodos] = useState([])
@@ -35,29 +37,31 @@ function TodoScreen() {
 
     async function pressHandler(title) {
         await todosRef.add({
-          title: title,
-          date: date,
-          month: month,
-          year: year
+            title: title,
+            date: date,
+            month: month,
+            year: year
         });
-      }
-    
-      async function deleteHandler(id) {
-         await db.collection("todos").doc(id).delete()
-      }
+    }
 
+    async function deleteHandler(id) {
+        await db.collection("todos").doc(id).delete()
+    }
+    console.log(todos)
 
     return (
         <div>
-            <Header dateItem={date}/>
+            <Header dateItem={date} />
             <div className='container'>
-                {todos.map(todo => {
+                {todos.length > 0 ? todos.map(todo => {
                     return (
                         <Card item={todo} deleteItem={deleteHandler} />
                     )
-                })}
+                }) :
+                    <Loader />
+                }
             </div>
-            <GoalInput  addHandler={pressHandler} deleteItem={deleteHandler}/>
+            <GoalInput addHandler={pressHandler} deleteItem={deleteHandler} />
         </div>
     )
 }
